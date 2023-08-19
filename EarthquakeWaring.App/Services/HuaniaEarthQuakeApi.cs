2c16e884b4cb63c63fcf0ae2c4a1cca89442b4ff
kengwang <atkengwang@qq.com>
2023-08-19 10:39:17 +0800

    [chore] 混淆内容

--- EarthquakeWaring.App/Services/HuaniaEarthQuakeApi.cs
+++ EarthquakeWaring.App/Services/HuaniaEarthQuakeApi.cs
@@ -3,8 +3,10 @@ using EarthquakeWaring.App.Infrastructure.Models.EarthQuakeModels;
 using EarthquakeWaring.App.Infrastructure.ServiceAbstraction;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using EarthquakeWaring.App.Infrastructure.Models.BaseModels;
@@ -16,7 +18,7 @@ public class HuaniaEarthQuakeApi : IEarthQuakeApi
     private readonly IHttpRequester _httpRequester;
     private readonly IJsonConvertService _jsonConvert;
     private readonly ILogger<HuaniaEarthQuakeApi> _logger;
-    private const string HuaniaApi = "https://mobile-new.chinaeew.cn/v1/";
+    private string HuaniaApi = Encoding.UTF8.GetString(System.Convert.FromBase64String("aHR0cHM6Ly9tb2JpbGUtbmV3LmNoaW5hZWV3LmNuL3YxLw=="));
 
     public HuaniaEarthQuakeApi(IHttpRequester httpRequester, IJsonConvertService jsonConvert,
         ILogger<HuaniaEarthQuakeApi> logger)
